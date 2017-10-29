58494cb0354accbcd1d8ac9ebc8d66a6481f7e62
alectramell <alectramell@gmail.com>
2017-10-29 11:41:50 -0700

    shadows

--- library.sh
+++ library.sh
@@ -29,11 +29,10 @@ clear
 if [ $MULA == "1" ]
 then
 	echo "${paper}${bold}${white}CONTACT PICK (${sky}1 of $HCOUNT${white}): ${reset}"
+	echo -n "${green}[${reset}"
+	echo -n 'PAY $20 USD'
+	echo -n "${green}]${reset}"
 	echo -n "${blue}[${reset}"
-	echo -n "${green}PAY${reset}"
-	echo -n '$20'
-	echo -n "${green}USD${reset}"
-	echo -n "${blue}][${reset}"
 	echo -n "${JOB[$MIX]}" | xxd -r -p
 	echo -n "${blue}]${reset}"
 	echo " " | pg
@@ -41,11 +40,10 @@ then
 elif [ $MULA == "2" ]
 then
 	echo "${paper}${bold}${white}CONTACT PICK (${sky}1 of $HCOUNT${white}): ${reset}"
+	echo -n "${green}[${reset}"
+	echo -n 'PAY $25 USD'
+	echo -n "${green}]${reset}"
 	echo -n "${blue}[${reset}"
-	echo -n "${green}PAY${reset}"
-	echo -n '$25'
-	echo -n "${green}USD${reset}"
-	echo -n "${blue}][${reset}"
 	echo -n "${JOB[$MIX]}" | xxd -r -p
 	echo -n "${blue}]${reset}"
 	echo " " | pg
@@ -53,11 +51,10 @@ then
 elif [ $MULA == "3" ]
 then
 	echo "${paper}${bold}${white}CONTACT PICK (${sky}1 of $HCOUNT${white}): ${reset}"
+	echo -n "${green}[${reset}"
+	echo -n 'PAY $10 USD'
+	echo -n "${green}]${reset}"
 	echo -n "${blue}[${reset}"
-	echo -n "${green}PAY${reset}"
-	echo -n '$10'
-	echo -n "${green}USD${reset}"
-	echo -n "${blue}][${reset}"
 	echo -n "${JOB[$MIX]}" | xxd -r -p
 	echo -n "${blue}]${reset}"
 	echo " " | pg
