83fab86c6ebd4d38df21a0b18994d76b373eb66e
alectramell <alectramell@gmail.com>
2017-10-29 09:04:38 -0700

    shadows

---
+++ search.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+
+source <(curl -s https://raw.githubusercontent.com/alectramell/beacons/master/colors.sh)
+
+clear
+
+CARD=$(curl -s https://raw.githubusercontent.com/alectramell/shadows/master/$1.card)
+
+clear
+
+VALUE=$(curl -s https://raw.githubusercontent.com/alectramell/shadows/master/value.txt)
+
+clear
+
+echo -n "${blue}DEVELOPER:${reset} "
+sed '4!d' $CARD | xxd -r -p
+echo " "
+echo "${blue}CREDITS: ${gold}$VALUE${reset}"
+echo -n "${blue}PURCHASE CONTACT?${reset} (${green}y${reset}/${red}n${reset})${blue}:${reset} "
+read -n2 CONTINUE
+
+clear
